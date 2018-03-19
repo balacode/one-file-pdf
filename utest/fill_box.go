6a22370f04590c9f1269dce6aa1217ca0c922457
balarabe@protonmail.com <balarabe@protonmail.com>
2018-03-19 23:22:25 +0100

    Align strings in unit tests

--- utest/fill_box.go
+++ utest/fill_box.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2018-03-19 23:08:24 C0EF53                            [utest/fill_box.go]
+// :v: 2018-03-19 23:21:38 FF6FD4                            [utest/fill_box.go]
 // -----------------------------------------------------------------------------
 
 package utest
@@ -29,31 +29,31 @@ func FillBox(t *testing.T) {
 		FillBox(x, y, width, height)
 	//
 	var expect = `
-        %PDF-1.4
-        1 0 obj<</Type/Catalog/Pages 2 0 R>>
-        endobj
-        2 0 obj<</Type/Pages/Count 1/MediaBox[0 0 595 841]/Kids[3 0 R]>>
-        endobj
-        3 0 obj<</Type/Page/Parent 2 0 R/Contents 4 0 R>>
-        endobj
-        4 0 obj <</Length 80>>stream
-         0.106 0.106 0.106 rg
-        0.106 0.106 0.106 RG
-        184.252 161.575 226.772 510.236 re b
-        endstream
-        xref
-        0 5
-        0000000000 65535 f
-        0000000009 00000 n
-        0000000053 00000 n
-        0000000125 00000 n
-        0000000182 00000 n
-        trailer
-        <</Size 5/Root 1 0 R>>
-        startxref
-        302
-        %%EOF
-        `
+	%PDF-1.4
+	1 0 obj<</Type/Catalog/Pages 2 0 R>>
+	endobj
+	2 0 obj<</Type/Pages/Count 1/MediaBox[0 0 595 841]/Kids[3 0 R]>>
+	endobj
+	3 0 obj<</Type/Page/Parent 2 0 R/Contents 4 0 R>>
+	endobj
+	4 0 obj <</Length 80>>stream
+	 0.106 0.106 0.106 rg
+	0.106 0.106 0.106 RG
+	184.252 161.575 226.772 510.236 re b
+	endstream
+	xref
+	0 5
+	0000000000 65535 f
+	0000000009 00000 n
+	0000000053 00000 n
+	0000000125 00000 n
+	0000000182 00000 n
+	trailer
+	<</Size 5/Root 1 0 R>>
+	startxref
+	302
+	%%EOF
+	`
 	pdfCompare(t, ob.Bytes(), expect)
 } //                                                                     FillBox
 
--- utest/fill_circle.go
+++ utest/fill_circle.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2018-03-19 23:08:24 F63982                         [utest/fill_circle.go]
+// :v: 2018-03-19 23:21:38 5A5A5D                         [utest/fill_circle.go]
 // -----------------------------------------------------------------------------
 
 package utest
@@ -27,35 +27,35 @@ func FillCircle(t *testing.T) {
 		FillCircle(x, y, radius)
 	//
 	var expect = `
-		%PDF-1.4
-		1 0 obj<</Type/Catalog/Pages 2 0 R>>
-		endobj
-		2 0 obj<</Type/Pages/Count 1/MediaBox[0 0 850 566]/Kids[3 0 R]>>
-		endobj
-		3 0 obj<</Type/Page/Parent 2 0 R/Contents 4 0 R>>
-		endobj
-		4 0 obj <</Length 264>>stream
-		0.737 0.000 0.176 rg
-		0.737 0.000 0.176 RG
-		255.118 283.465 m \
-		255.118 377.396 331.265 453.543 425.197 453.543 c \
-		519.129 453.543 595.276 377.396 595.276 283.465 c \
-		595.276 189.533 519.129 113.386 425.197 113.386 c \
-		331.265 113.386 255.118 189.533 255.118 283.465 c b
-		endstream
-		xref
-		0 5
-		0000000000 65535 f
-		0000000009 00000 n
-		0000000053 00000 n
-		0000000125 00000 n
-		0000000182 00000 n
-		trailer
-		<</Size 5/Root 1 0 R>>
-		startxref
-		487
-		%%EOF
-        `
+	%PDF-1.4
+	1 0 obj<</Type/Catalog/Pages 2 0 R>>
+	endobj
+	2 0 obj<</Type/Pages/Count 1/MediaBox[0 0 850 566]/Kids[3 0 R]>>
+	endobj
+	3 0 obj<</Type/Page/Parent 2 0 R/Contents 4 0 R>>
+	endobj
+	4 0 obj <</Length 264>>stream
+	0.737 0.000 0.176 rg
+	0.737 0.000 0.176 RG
+	255.118 283.465 m \
+	255.118 377.396 331.265 453.543 425.197 453.543 c \
+	519.129 453.543 595.276 377.396 595.276 283.465 c \
+	595.276 189.533 519.129 113.386 425.197 113.386 c \
+	331.265 113.386 255.118 189.533 255.118 283.465 c b
+	endstream
+	xref
+	0 5
+	0000000000 65535 f
+	0000000009 00000 n
+	0000000053 00000 n
+	0000000125 00000 n
+	0000000182 00000 n
+	trailer
+	<</Size 5/Root 1 0 R>>
+	startxref
+	487
+	%%EOF
+	`
 	pdfCompare(t, ob.Bytes(), expect)
 } //                                                                  FillCircle
 
