2891f1409c9831b73a0b6f5446e261e2f57b6ecc
balarabe@protonmail.com <balarabe@protonmail.com>
2018-03-22 03:44:58 +0100

    Create demo/demo_test.go

---
+++ demo/demo_test.go
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------------
+// (c) balarabe@protonmail.com                                      License: MIT
+// :v: 2018-03-22 03:44:02 F14590                                 [demo_test.go]
+// -----------------------------------------------------------------------------
+
+package main
+
+import "testing" // standard
+
+func TestDemos(t *testing.T) {
+	main()
+} //                                                                   TestDemos
+
+//end
